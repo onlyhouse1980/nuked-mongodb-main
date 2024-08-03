761f653d044d460ce5ccb9729407c70f5f4bae9f
onlyhouse1980 <56930138+onlyhouse1980@users.noreply.github.com>
2024-08-03 22:07:55 +0200

    Update [last_name].js

--- src/pages/users/[last_name].js
+++ src/pages/users/[last_name].js
@@ -1947,6 +1947,56 @@ function August2024() {
             </thead>
             <tbody>
 
+ {/* June 2024 */}
+              <tr>
+                <td className={styles.td3}>
+                  <p className={styles.p}>Jun 2024</p>
+                  <p
+                    style={{
+                      fontSize: 9,
+                      width: "75px",
+                      color: "white",
+                      margin: "-20px 0 0 10px",
+                      padding: 0,
+                    }}
+                  >
+                    (6/1/24) - (8/1/24)
+                  </p>
+                </td>
+                <td className={styles.td3}>
+                  <input
+                    style={{
+                      backgroundColor: bgColors.Blue,
+                      margin: "0 10px 0 10px",
+                      width: "75px",
+                    }}
+                    type="text"
+                    name="utilized"
+                    id="June2024"
+                    value={(data.aug01_24 - data.jun01_24)}
+                    label="answers"
+                    readOnly
+                  ></input>
+                </td>
+                <td className={styles.td3}>
+                  <input
+                    style={{
+                      backgroundColor: bgColors.Blue,
+                      margin: "0 10px 0 10px",
+                      width: "75px",
+                    }}
+                    type="text"
+                    name="utilized"
+                    id="June2024"
+                    value={(6000 - (data.aug01_24 - data.jun01_24))*-1}
+                    label="answers"
+                    readOnly
+                  ></input>
+                </td>
+                <td className={styles.td3}>{August2024()}</td>
+              </tr>
+
+          
               {/* April 2024 */}
               <tr>
                 <td className={styles.td3}>
