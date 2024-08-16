161b1f3888c2b0dd85c5aefba917b7c6e593622f
onlyhouse1980 <56930138+onlyhouse1980@users.noreply.github.com>
2024-08-16 22:45:24 +0200

    Update 2024_minutes.js

--- src/pages/pdfs/2024_minutes.js
+++ src/pages/pdfs/2024_minutes.js
@@ -1,1 +1,20 @@
-https://drive.google.com/file/d/1E6QSZxqgehmMSoql4ZiTtE78fiq2FKiu/view?usp=sharing
+import styles from "../../styles/pdfs.module.css";
+import React, { useState } from "react";
+export const App = () => {
+  const [embedURL] = useState(
+    "https://drive.google.com/file/d/1E6QSZxqgehmMSoql4ZiTtE78fiq2FKiu/preview"
+  );
+  return (
+    <div className="container">
+      <h5>2023 Brd. Appt. Minutes</h5>
+      <iframe
+        className={styles.iframe}
+        src={embedURL}
+        width="100%"
+        height="500"
+      ></iframe>
+    </div>
+  );
+};
+
+export default App;
