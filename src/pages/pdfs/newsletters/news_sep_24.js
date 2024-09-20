9da05d4eeaaa3d2d325347d22a598a75571ea3dc
onlyhouse1980 <56930138+onlyhouse1980@users.noreply.github.com>
2024-09-20 17:34:42 +0200

    Create news_sep_24.js

---
+++ src/pages/pdfs/newsletters/news_sep_24.js
@@ -0,0 +1,20 @@
+import React, { useState } from "react";
+import styles from "../../../styles/pdfs.module.css";
+
+export const App = () => {
+  const [embedURL] = useState(
+    "https://drive.google.com/file/d/1aKKW-g3vlgATw0oujXlSlz93gf-BDXLl/preview"
+  );
+  return (
+    <div className="container">
+      <iframe
+        className={styles.iframPDF}
+        src={embedURL}
+        width="100%"
+        height="1000px"
+      ></iframe>
+    </div>
+  );
+};
+
+export default App;
