87bb0042879d399cfbf1b6c34dcfc7fe8fd1fc5f
onlyhouse1980 <56930138+onlyhouse1980@users.noreply.github.com>
2024-09-20 05:11:56 +0200

    Update [meter_serialNum].js

--- src/pages/api/people/[meter_serialNum].js
+++ src/pages/api/people/[meter_serialNum].js
@@ -1,20 +1,25 @@
-import { people } from '../../../../data'
+// pages/api/people/[meter_serialNum].js
+import clientPromise from '../../../../lib/mongodb';
 
+export default async function handler(req, res) {
+  const client = await clientPromise;
+  const db = client.db('mydatabase');
 
-export default function personHandler({ query: { meter_serialNum } }, res) {
-  try {
+  const { meter_serialNum } = req.query;
 
-    const person = people.find((p) => p.meter_serialNum === meter_serialNum)
-
-    // User with meter_serialNum exists
-    if (person) {
-      res.status(200).json(person)
-    } else {
-      const sanitizedMeterSerialNum = sanitize(meter_serialNum);
-
-      res.status(404).json({ message: `User with meter_serialNum: ${meter_serialNum} not found.` })
+  if (req.method === 'GET') {
+    try {
+      const user = await db.collection('users').findOne({ meter_serialNum: meter_serialNum });
+      if (user) {
+        res.status(200).json({ status: 200, data: user });
+      } else {
+        res.status(404).json({ status: 404, message: 'Serial not found' });
+      }
+    } catch (error) {
+      res.status(500).json({ status: 500, message: 'Internal server error' });
     }
-  } catch (error) {
-    res.status(500).json({ message: "An error occurred while processing the request." })
+  } else {
+    res.setHeader('Allow', ['GET']);
+    res.status(405).end(`Method ${req.method} Not Allowed`);
   }
 }
