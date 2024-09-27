3581bde2f4af5947aa3f9b8fa8c0c848927ab86b
onlyhouse1980 <56930138+onlyhouse1980@users.noreply.github.com>
2024-09-27 14:13:33 +0200

    Create 2021annualminutes.js

---
+++ src/pages/pdfs/misc/minutes/2021annualminutes.js
@@ -0,0 +1,21 @@
+import React, { useState } from "react";
+import styles from "./2023Minutes.module.css";
+
+export const App = () => {
+  const [embedURL] = useState(
+    "https://drive.google.com/file/d/1vZs0nYPFSMLJtXhJBxeuJK5BNEpN4zBW/preview"
+  );
+  return (
+    <div className={styles.container}>
+      <iframe
+        className={styles.iframe}
+        src={embedURL}
+        width="100%"
+        height="500"
+      ></iframe>
+    </div>
+  );
+};
+
+export default App;
+
