3b9c9622cabb973b5d9132e48f14f9ec47bed5be
onlyhouse1980 <56930138+onlyhouse1980@users.noreply.github.com>
2024-10-30 05:20:35 +0100

    Update input.js

--- src/pages/spreadsheet/input.js
+++ src/pages/spreadsheet/input.js
@@ -1,10 +1,13 @@
-// pages/index.js
+// pages/spreadsheet/input.js
 import Spreadsheet from '../../components/Spreadsheet';
-
+import styles from './input.module.css'
 const Home = () => (
-  <div>
+  <div className={styles.horizscroll}>
     <h1>OBCG Meter Readings</h1>
+    <div className= {styles.scroll}>
+    
     <Spreadsheet />
+    </div>
   </div>
 );
 
