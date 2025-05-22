e48ae5f20ca2efd9f6aef70e2947472a13c6f8c8
onlyhouse1980 <56930138+onlyhouse1980@users.noreply.github.com>
2025-05-22 12:26:12 +0200

    Create PUDVideoComponent.jsx

---
+++ src/components/PUDVideoComponent.jsx
@@ -0,0 +1,5 @@
+export default async function VideoComponent() {
+  const src = await getVideoSrc()
+ 
+  return <iframe src="https://drive.google.com/file/d/1Op6BCXirdL5YD0YOXRKE890U7uzvESOf/preview" allowFullScreen />
+}
