7ff2ff5865caf2336f1b062288dd0d14006005a6
onlyhouse1980 <56930138+onlyhouse1980@users.noreply.github.com>
2025-06-24 17:13:05 +0200

    Update executive.js

--- src/pages/executive.js
+++ src/pages/executive.js
@@ -66,7 +66,7 @@ const Executive = () => (
             <p>
               Vice President
               <br />
-              Erika Aust
+              Eric Campbell
               <br />
               
             </p>
