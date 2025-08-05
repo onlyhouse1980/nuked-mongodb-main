ace3b9e0b187b669641869d8bdf14dde13683ade
onlyhouse1980 <56930138+onlyhouse1980@users.noreply.github.com>
2025-08-05 09:10:04 +0000

    member stuff

--- src/pages/forgot-password.js
+++ src/pages/forgot-password.js
@@ -3,6 +3,15 @@
 import { useState } from "react";
 import Head from "next/head";
 import { BiEnvelope } from "react-icons/bi";
+import { Button } from "../../components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+  CardFooter,
+} from "../../components/ui/card";
 
 export default function ForgotPassword() {
   const [email, setEmail] = useState("");
@@ -45,69 +54,60 @@ export default function ForgotPassword() {
 
   return (
     <>
-      <Head>
-        <title>Forgot Password</title>
-      </Head>
+      <Card className="card">
+        <CardHeader>
+          <title>Forgot Password</title>
+        </CardHeader>
 
-      <div className="flex items-center justify-center min-h-screen bg-gray-100 p-4 sm:p-6 lg:p-8">
-        <div className="bg-white p-6 sm:p-8 rounded-xl shadow-2xl w-full max-w-sm transform transition duration-500 hover:scale-105">
-          <div className="flex flex-col items-center mb-6">
+        <div className="flex items-center justify-center min-h-screen bg-gray-100 p-4 sm:p-6 lg:p-8">
+          <div className="bg-white p-6 sm:p-8 rounded-xl shadow-2xl w-full max-w-sm transform transition duration-500 hover:scale-105">
+            {" "}
             <BiEnvelope className="text-indigo-600 text-5xl sm:text-6xl mb-2" />
-            <h1 className="text-2xl sm:text-3xl font-extrabold text-gray-900">
-              Forgot Password
-            </h1>
+            <h1>Forgot Password</h1>
           </div>
+          <div>
+            <p className="text-sm text-gray-600 text-center mb-6">
+              Enter your email to receive a password reset link.
+            </p>
 
-          <p className="text-sm text-gray-600 text-center mb-6">
-            Enter your email to receive a password reset link.
-          </p>
+            <form onSubmit={handleSubmit} className="space-y-4">
+              <div>
+                <input
+                  type="email"
+                  id="email"
+                  name="email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  placeholder="you@example.com"
+                  required
+                  className="mt-1 block w-full px-4 py-2 bg-gray-50 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm transition duration-150 ease-in-out"
+                />
+              </div>
 
-          <form onSubmit={handleSubmit} className="space-y-4">
-            <div>
-              <label
-                htmlFor="email"
-                className="block text-sm font-medium text-gray-700"
+              <button
+                type="submit"
+                disabled={isLoading}
+                className={"my-button"}
               >
-                Email Address
-              </label>
-              <input
-                type="email"
-                id="email"
-                name="email"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                placeholder="you@example.com"
-                required
-                className="mt-1 block w-full px-4 py-2 bg-gray-50 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm transition duration-150 ease-in-out"
-              />
-            </div>
-
-            <button
-              type="submit"
-              disabled={isLoading}
-              className={`w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white transition-colors duration-150 ease-in-out ${
-                isLoading
-                  ? "bg-indigo-400 cursor-not-allowed"
-                  : "bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-              }`}
-            >
-              {isLoading ? "Sending..." : "Send Reset Link"}
-            </button>
-          </form>
-
-          {message && (
-            <div
-              className={`mt-6 p-4 rounded-md text-sm text-center ${
-                isError
-                  ? "bg-red-100 text-red-700"
-                  : "bg-green-100 text-green-700"
-              }`}
-            >
-              {message}
-            </div>
-          )}
+                {isLoading ? "Sending..." : "Send Reset Link"}
+              </button>
+            </form>
+          </div>
+          <CardFooter>
+            {message && (
+              <div
+                className={`mt-6 p-4 rounded-md text-sm text-center ${
+                  isError
+                    ? "bg-red-100 text-red-700"
+                    : "bg-green-100 text-green-700"
+                }`}
+              >
+                {message}
+              </div>
+            )}
+          </CardFooter>
         </div>
-      </div>
+      </Card>
     </>
   );
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,95 +1,112 @@
-import Image from 'next/image'
-import Head from 'next/head';
-import styles from '../styles/styles.module.css'
+import Image from "next/image";
+import Head from "next/head";
+import styles from "../styles/styles.module.css";
 import * as React from "react";
-import { motion, inView} from "framer-motion";
-import profileImage from "../../public/Images/WebPFiles/psound.webp"
-import Marquee from "react-fast-marquee"
-import Link from "next/link"
+import { motion, inView } from "framer-motion";
+import profileImage from "../../public/Images/WebPFiles/psound.webp";
+import Marquee from "react-fast-marquee";
+import Link from "next/link";
+import { Button } from "../../components/ui/button";
 
-const PhotoArray = []
+const PhotoArray = [];
 
-export default function index(){
-  return(
-    
-  <div>
-    <Head>
+export default function index() {
+  return (
+    <div>
+      <Head>
         <title>Orchard Beach Community Group</title>
-        <meta property="og:title" content="Orchard Beach Community Group" key="title" /> 
-    </Head>
-    
-     <div className={styles.mainName}>
-    <h2 className={styles.bgText}>
-      <b>Orchard Beach</b>
-    </h2>
-    <h2 className={styles.bgText}>
-      <b>Community Group</b>
-    </h2>{/*
-  <Marquee className='z=100 drop-shadow-md'>
-      <h5 className='text-white z=100 drop-shadow-md'>Annual Meeting! Saturday, June 21, 2025 - 2 to 3 p.m.  Location: 340 E Orchard Beach Drive&nbsp;</h5>
-    </Marquee>*/}
-    </div>
-  <div>
-    <motion.div exit={{ opacity: 0 }} initial={{opacity: 0 }} animate={{opacity: 1 }}>
-    
-    <motion.div animate={{
-        opacity: [1, 0, 0, 0, 1],        
-      }}
-      transition={{
-        duration: 30,
-        ease: "easeInOut",
-        
-        repeat: Infinity,
-        repeatDelay: 0
-      }} 
-      className={styles.bgWrap}>
-      <Image
-        className={styles.Psound}
-        alt="Whale Sighting"
-        src="/Images/WebPFiles/psound.webp"
-        layout="fill"
-        objectFit="cover"
-        quality={85}
-      />
-    </motion.div>
-    <motion.div animate={{
-        opacity: [0, 1, 0, 0, 0],       
-      }}
-      transition={{
-        duration: 30,
-        ease: "easeInOut",
-        repeat: Infinity,
-        repeatDelay: 0
-      }} 
-      className={styles.bgWrap}>
-      <Image
-        alt="Sunrise"
-        src="/Images/WebPFiles/sunrise.webp"
-        layout="fill"
-        objectFit="cover"
-        quality={50}
-      />
-    </motion.div>
-    <motion.div animate={{
-        opacity: [0, 0, 1, 0, 0],        
-      }}
-      transition={{
-        duration: 30,
-        ease: "easeInOut",
-        repeat: Infinity,
-        repeatDelay: 0
-      }} 
-      className={styles.bgWrap}>
-      <Image
-        className={styles.Psound}
-        alt="Droplets"
-        src="/Images/WebPFiles/boat.webp"
-        layout="fill"
-        objectFit="cover"
-        quality={85}
-      />
-    </motion.div>
-    {/* <motion.div animate={{
+        <meta
+          property="og:title"
+          content="Orchard Beach Community Group"
+          key="title"
+        />
+      </Head>
+
+      <div className={styles.mainName}>
+        <h2 className={styles.bgText}>
+          <b>Orchard Beach</b>
+        </h2>
+        <h2 className={styles.bgText}>
+          <b>Community Group</b>
+        </h2>
+        <Marquee className="z=100 drop-shadow-md">
+          <h5 className="text-white z=100 drop-shadow-md">
+            NEW! <Link href="/login">Login</Link> or{" "}
+            <Link href="/signup">Signup</Link> now available. Get all your usage
+            and billing information online! &nbsp;
+          </h5>
+        </Marquee>
+      </div>
+      <div>
+        <motion.div
+          exit={{ opacity: 0 }}
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+        >
+          <motion.div
+            animate={{
+              opacity: [1, 0, 0, 0, 1],
+            }}
+            transition={{
+              duration: 30,
+              ease: "easeInOut",
+
+              repeat: Infinity,
+              repeatDelay: 0,
+            }}
+            className={styles.bgWrap}
+          >
+            <Image
+              className={styles.Psound}
+              alt="Whale Sighting"
+              src="/Images/WebPFiles/psound.webp"
+              layout="fill"
+              objectFit="cover"
+              quality={85}
+            />
+          </motion.div>
+          <motion.div
+            animate={{
+              opacity: [0, 1, 0, 0, 0],
+            }}
+            transition={{
+              duration: 30,
+              ease: "easeInOut",
+              repeat: Infinity,
+              repeatDelay: 0,
+            }}
+            className={styles.bgWrap}
+          >
+            <Image
+              alt="Sunrise"
+              src="/Images/WebPFiles/sunrise.webp"
+              layout="fill"
+              objectFit="cover"
+              quality={50}
+            />
+          </motion.div>
+          <motion.div
+            animate={{
+              opacity: [0, 0, 1, 0, 0],
+            }}
+            transition={{
+              duration: 30,
+              ease: "easeInOut",
+              repeat: Infinity,
+              repeatDelay: 0,
+            }}
+            className={styles.bgWrap}
+          >
+            <Image
+              className={styles.Psound}
+              alt="Droplets"
+              src="/Images/WebPFiles/boat.webp"
+              layout="fill"
+              objectFit="cover"
+              quality={85}
+            />
+          </motion.div>
+          {/* <motion.div animate={{
         opacity: [0, 0, 0, 0, 0],        
       }}
       transition={{
@@ -107,47 +124,49 @@ export default function index(){
         quality={85}
       />
     </motion.div> */}
-    <motion.div animate={{
-        opacity: [0, 0, 0, 1, 0],        
-      }}
-      transition={{
-        duration: 30,
-        ease: "easeInOut",
-        repeat: Infinity,
-        repeatDelay: 0
-      }} 
-      className={styles.bgWrap}>
-      <Image
-        alt="Orchard Beach"
-        src="/Images/WebPFiles/hero-bg.webp"
-        layout="fill"
-        objectFit="cover"
-        quality={85}
-      />
-    </motion.div>
-    <motion.div animate={{
-        opacity: [0, 0, 0, 0, 1],        
-      }}
-      transition={{
-        duration: 30,
-        ease: "easeInOut",
-        repeat: Infinity,
-        repeatDelay: 0
-      }} 
-      className={styles.bgWrap}>
-      <Image
-        alt="Water Droplets"
-        src="/Images/WebPFiles/glass_droplets.webp"
-        /*filter="blur"*/
-        layout="fill"
-        objectFit="cover"
-        quality={85}
-      />
-    </motion.div>
-   
-    
-    </motion.div>
-
+          <motion.div
+            animate={{
+              opacity: [0, 0, 0, 1, 0],
+            }}
+            transition={{
+              duration: 30,
+              ease: "easeInOut",
+              repeat: Infinity,
+              repeatDelay: 0,
+            }}
+            className={styles.bgWrap}
+          >
+            <Image
+              alt="Orchard Beach"
+              src="/Images/WebPFiles/hero-bg.webp"
+              layout="fill"
+              objectFit="cover"
+              quality={85}
+            />
+          </motion.div>
+          <motion.div
+            animate={{
+              opacity: [0, 0, 0, 0, 1],
+            }}
+            transition={{
+              duration: 30,
+              ease: "easeInOut",
+              repeat: Infinity,
+              repeatDelay: 0,
+            }}
+            className={styles.bgWrap}
+          >
+            <Image
+              alt="Water Droplets"
+              src="/Images/WebPFiles/glass_droplets.webp"
+              /*filter="blur"*/
+              layout="fill"
+              objectFit="cover"
+              quality={85}
+            />
+          </motion.div>
+        </motion.div>
+      </div>
     </div>
-    </div>
-)}
+  );
+}
