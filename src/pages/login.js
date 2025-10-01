62982208858670eece5be46871d076a07ad34c44
onlyhouse1980 <56930138+onlyhouse1980@users.noreply.github.com>
2025-10-01 17:15:55 +0200

    Update login.js

--- src/pages/login.js
+++ src/pages/login.js
@@ -27,10 +27,6 @@ export default function Login() {
   const [isLoading, setIsLoading] = useState(false);
   const router = useRouter();
 
-    if (result.status === "authenticated") {
-    router.push("/dashboard");
-    return null;
-  }
 
   const handleSubmit = async (event) => {
     event.preventDefault();
