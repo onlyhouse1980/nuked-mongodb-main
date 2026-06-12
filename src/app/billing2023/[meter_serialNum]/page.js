8a53272647bd9d89db9488e6e4c778b45df312fe
Onlyhouse1980 <ryan@ryanernstnyberg.com>
2026-06-12 02:28:32 +0200

    declutter

--- src/app/billing2023/[meter_serialNum]/page.js
+++
@@ -1,5 +0,0 @@
-import Component from '@/routes/billing2023/[meter_serialNum]';
-
-export default function Page() {
-  return <Component />;
-}
--- src/app/countdown/[last_name]/page.js
+++
@@ -1,5 +0,0 @@
-import Component from '@/routes/countdown/[last_name]';
-
-export default function Page() {
-  return <Component />;
-}
