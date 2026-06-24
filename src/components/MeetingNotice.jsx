3d1dfca0f65b9879863ce4e9301b3451971e1563
Ryan Nyberg <56930138+onlyhouse1980@users.noreply.github.com>
2026-06-24 16:17:53 +0200

    Add MeetingNotice component for annual meeting details

---
+++ src/components/MeetingNotice.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function MeetingNotice() {
+  return (
+    <h5 style={{ color: "#ffffff" }}>
+      ANNUAL MEETING - We will meet at 11 a.m. Saturday, June 27 at the Orchard Beach Public Park (the turnaroundat the south end of Orchard Beach Drive)  
+    </h5>
+  );
+}
