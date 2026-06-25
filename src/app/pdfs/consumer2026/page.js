af6e3819f26a0f4c5f0b94b2b8c7619a366fbe6d
Ryan Nyberg <56930138+onlyhouse1980@users.noreply.github.com>
2026-06-25 16:22:52 +0200

    Add Consumer2025 component to embed Google Drive file

---
+++ src/app/pdfs/consumer2026/page.js
@@ -0,0 +1,10 @@
+export default function Consumer2025() {
+  const embedURL =
+    "https://drive.google.com/file/d/1vZCjR-oFGNntHi5RuRSJT5w2QoYT7ZR5/preview";
+  return (
+    <div className="container">
+      <h5>Consumer Confidence 2025</h5>
+      <iframe src={embedURL} width="100%" height="500" />
+    </div>
+  );
+}
